d91988972cb202f0a1370e8dc64be9eaa9976d51
Gurpreet Shah <shah_gurpreet@yahoo.in>
2016-01-29 16:50:22 -0500

    created public directory moved index.html under public directory

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
     res.send('hello world experiment');
 });
+app.get('/api/users', function(req, res) {
+    var users = [
+        {username: 'alice12', name: 'alice'},
+        {username: 'bob23', name:'bob'}
+    ];
+    res.send(users);
+});
+
+//Serve static content for the app from the “public” directory in the application directory
+app.use(express.static(__dirname + '/public'));
+
 app.listen(port, ipaddress);
