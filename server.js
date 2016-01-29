0bd9b0430a95e802dc041ca473ba111e6aaa361d
Gurpreet Shah <shah_gurpreet@yahoo.in>
2016-01-29 15:39:17 -0500

    changed server.js added files to be ignored to gitignore

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
-    res.send('hello world');
+    res.send('hello world experiment');
 });
 app.listen(port, ipaddress);
