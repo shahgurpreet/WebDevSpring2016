e68dd6c341b123b85a15c4b83bb0df5e21c683a5
Gurpreet Shah <shah_gurpreet@yahoo.in>
2016-07-22 13:19:15 -0400

    added resume

--- public/project/server/services/instagram.service.server.js
+++ public/project/server/services/instagram.service.server.js
@@ -6,32 +6,40 @@ module.exports = function(app) {
     var https = require('https');
     var slug = require('limax');
 
-    var Flickr = require("node-flickr");
-    var keys = {
-        api_key: "623349722c4d246607fb689c500b3c7e",
-        secret: "e4dc1d532cd61c14"
-    };
+    var Flickr = require("flickrapi"),
+        flickrOptions = {
+            api_key: "623349722c4d246607fb689c500b3c7e",
+            secret: "e4dc1d532cd61c14"
+        };
 
-    var flickr = new Flickr(keys);
-
-    app.get('/api/instagram/:tag/:token', fetchFlickrPhotos);
+    app.get('/api/instagram/:tag/:token', newf);
 
 
 
-    function fetchFlickrPhotos(req, res) {
+    function newf(req, res) {
 
         var tag = req.params.tag;
         tag = slug(tag);
-        var token = req.params.token;
-        if(token === '0') {
-            token = '1';
-        }
-
-        flickr.get("photos.search", {"tags":tag, page: token}, function(err, result) {
-            if (err) return console.error(err);
-            res.send(result);
+        Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+            var token = req.params.token;
+            if(token === '0') {
+                token = '1';
+            }
+            if(error) {
+                console.log(error);
+            } else {
+                flickr.photos.search({
+                    text: tag,
+                    page: token
+                }, function(err, result) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        res.send(result);
+                    }
+                });
+            }
         });
-
     }
 
 
